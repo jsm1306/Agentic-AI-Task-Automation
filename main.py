3cf3e2a5d9f49cf8fbd58c43bb886992a56afb0e
jsm1306 <sindhu.j1729@gmail.com>
2026-01-14 17:00:53 +0530

    Tested File Manager Tool

--- main.py
+++ main.py
@@ -1,34 +1,17 @@
 from crewai import Task, Crew
-from agents.search_agent import search_agent
-from crewai import Agent
+from agents.file_agent import file_agent
 
-planner = Agent(
-    role="Planner",
-    goal="Break the user request into steps",
-    backstory="Expert planner",
-    llm="gemini/gemini-2.5-flash"
+task = Task(
+    description="Write a short note about AI agents and save it to notes",
+    expected_output="Confirmation that the note was saved",
+    agent=file_agent
 )
-
-task1 = Task(
-    description="Create a research plan about breast cancer",
-    expected_output="A detailed research plan for studying breast cancer",
-    agent=planner
-)
-
-task2 = Task(
-    description="Use web search to find recent information about pests classification in Castor crop in 2024",
-    expected_output="Recent articles and key data on pests classification in Castor crop in 2024",
-    agent=search_agent
-)
-
 
 crew = Crew(
-    agents=[planner, search_agent],
-    tasks=[task1, task2]
+    agents=[file_agent],
+    tasks=[task],
+    max_rpm=1   # extremely safe
 )
 
 result = crew.kickoff()
-print("Task 1 Output (Breast Cancer Research Plan):")
-print(task1.output)
-print("\nTask 2 Output (Pests Classification in Castor Crop):")
-print(task2.output)
+print(result)
