c73d6b3524f6d7f588d2d3c6fef2e300f79d1138
jsm1306 <sindhu.j1729@gmail.com>
2026-01-14 18:42:07 +0530

    Added Recovery System

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from crewai import Task, Crew
 from agents.memory_agent import memory_agent
+from agents.recovery_agent import recovery_agent
+from agents.self_correct_agent import self_correct_agent
 
-task = Task(
-    description="Recall everything you remember",
-    expected_output="List of stored memories",
-    agent=memory_agent
+task1 = Task(
+    description="Simulate failure: Search tool returned empty result",
+    expected_output="Failure recorded",
+    agent=recovery_agent
 )
-
+task2 = Task(
+    description="Based on failure, create a new improved plan",
+    expected_output="Recovery plan created",
+    agent=self_correct_agent
+)
 crew = Crew(
-    agents=[memory_agent],
-    tasks=[task],
+    agents=[recovery_agent,self_correct_agent],
+    tasks=[task1, task2],
     max_rpm=1,
     process="sequential",
     verbose=False
